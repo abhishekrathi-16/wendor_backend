744b292923c51dca2c36e0cc4fd9e2cf763e0dc5
mytestaccount-1 <abhishekrathi639@gmail.com>
2024-08-14 11:11:31 +0530

    update cors and admin controller

--- app.js
+++ app.js
@@ -1,70 +1,90 @@
-require('dotenv').config()
-require('express-async-errors')
-const path = require('path')
+require("dotenv").config();
+require("express-async-errors");
+const path = require("path");
 const express = require("express");
 const app = express();
-const morgan = require('morgan')
-const cookieParser = require("cookie-parser")
-const fileUpload = require("express-fileupload")
-const rateLimiter = require('express-rate-limit')
-const helmet = require('helmet')
-const xss = require('xss-clean')
-const cors = require('cors')
-const mongoSanitize = require('express-mongo-sanitize')
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
 
 // database
-const connectDB = require('./db/connect')
+const connectDB = require("./db/connect");
 
 // routers
-const authRouter = require('./routes/authRoutes')
-const adminRouter = require('./routes/adminRoutes')
-const shopperRouter = require('./routes/shopperRoutes')
+const authRouter = require("./routes/authRoutes");
+const adminRouter = require("./routes/adminRoutes");
+const shopperRouter = require("./routes/shopperRoutes");
 
-// middleware 
-const notFoundMiddleware = require('./middleware/not-found')
-const errorHandlerMiddleware = require('./middleware/error-handler')
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.set('trust proxy',1)
-app.use(rateLimiter({
-  windowMs: 15*60*1000,
-  max: 60
-}))
-app.use(helmet())
-app.use(cors())
-app.use(xss())
-app.use(mongoSanitize())
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
-app.use(morgan('tiny')) // tracks the requests made by a user to the server along with other data
-app.use(express.json())
-app.use(cookieParser(process.env.JWT_SECRET))
-// app.use(express.static('./public'))
-app.use(fileUpload())
+app.use(morgan("tiny")); // tracks the requests made by a user to the server along with other data
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+app.use(express.static("./public"));
+app.use(fileUpload());
+
+const allowedOrigins = ["http://localhost:3000","https://vendor-frontend-nine.vercel.app/"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Enable credentials (cookies, authorization headers)
+  })
+);
+
 // routes
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/public/index.html'))
-})
-app.get('/api',(req,res)=>{
-    // console.log(req.cookies)
-    // console.log(req.signedCookies)
-    res.send('Wendor Assignment Api')
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.get("/api", (req, res) => {
+  // console.log(req.cookies)
+  // console.log(req.signedCookies)
+  res.send("Wendor Assignment Api");
+});
 
-app.use('/api/auth',authRouter);
-app.use('/api/admin',adminRouter);
-app.use('/api/shopper',shopperRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/shopper", shopperRouter);
 
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
-    app.listen(port,'0.0.0.0' ,console.log(`Server is listening on port ${port}`));
+    await connectDB(process.env.MONGO_URI);
+    app.listen(
+      port,
+      "0.0.0.0",
+      console.log(`Server is listening on port ${port}`)
+    );
   } catch (error) {
     console.log(error);
   }
 };
 
-start()
-module.exports = app
+start();
+module.exports = app;
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,53 +1,74 @@
 const Inventory = require("../models/inventory");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
-const { notifyInventoryUpdate } = require("../utils")
+const { notifyInventoryUpdate } = require("../utils");
 
 // to create inventory in bulk
 const createInBulk = async (req, res) => {
-  const { items } = req.body;
-  
-  // this just adds new items to the database irrespective of whether they are already present in the database or not
-  // const createdItems = await Inventory.insertMany(items);
-
-  for (const item of items) {
-    const { name, price, available_units, display_image_url } = item;
-    const existingItem = await Inventory.findOne({ name });
-    if (existingItem) {
-      // if the item is already present in the database, update the available units
-      existingItem.available_units += available_units;
-      existingItem.price = price || existingItem.price;
-      existingItem.display_image_url =
-        display_image_url || existingItem.display_image_url;
-      await existingItem.save();
-    } else {
-      // if the item is not present in the database, add it
-      await Inventory.create({
-        name,
-        price,
-        available_units,
-        display_image_url,
-      });
+  try {
+    const { items } = req.body;
+    if (!Array.isArray(items)) {
+      throw new CustomError.BadRequestError(
+        "The items property must be an array."
+      );
     }
+
+    // this just adds new items to the database irrespective of whether they are already present in the database or not
+    // const createdItems = await Inventory.insertMany(items);
+
+    for (const item of items) {
+      const { name, price, available_units, display_image_url } = item;
+      const existingItem = await Inventory.findOne({ name });
+      if (existingItem) {
+        // if the item is already present in the database, update the available units
+        existingItem.available_units += Number(available_units);
+        existingItem.price = price || existingItem.price;
+        existingItem.display_image_url =
+          display_image_url || existingItem.display_image_url;
+        await existingItem.save();
+      } else {
+        // if the item is not present in the database, add it
+        await Inventory.create({
+          name,
+          price,
+          available_units,
+          display_image_url,
+        });
+      }
+    }
+    notifyInventoryUpdate();
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: "Items added successfully" });
+  } catch (err) {
+    console.log("Bulk create error", err);
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "Internal Server Error", error: error.message });
   }
-  notifyInventoryUpdate();
-  res.status(StatusCodes.CREATED).json({ message: "Items added successfully" });
 };
 
 // update item
 const updateItem = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const item = await Inventory.findOneAndUpdate({ _id: id }, req.body, {
-    new: true,
-    runValidators: true,
-  });
-  if (!item) {
-    throw new CustomError.NotFoundError(`No item found with id: ${id}`);
+    const item = await Inventory.findOneAndUpdate({ _id: id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!item) {
+      throw new CustomError.NotFoundError(`No item found with id: ${id}`);
+    }
+    notifyInventoryUpdate();
+
+    res.status(StatusCodes.OK).json({ item });
+  } catch (err) {
+    console.log("update item error", err);
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "Internal Server Error", error: error.message });
   }
-  notifyInventoryUpdate();
-
-  res.status(StatusCodes.OK).json({ item });
 };
 
 module.exports = { createInBulk, updateItem };
